22920375ec60ff55a5293fa4cd6ed9b6e6080a72
Liaodar <2325131033@qq.com>
2026-07-16 18:25:00 +0800

    坤坤跳舞（没有音乐）

---
+++ 9-3，坤坤跳舞/Delay.c
@@ -0,0 +1,15 @@
+
+void Delay(xms)		//@12.000MHz
+{
+	unsigned char i, j;
+
+	
+	while(xms--)
+{	i = 2;
+	j = 239;
+	do
+	{
+		while (--j);
+		} while (--i);}
+}
+
---
+++ 9-3，坤坤跳舞/Matrix.h
@@ -0,0 +1,8 @@
+#ifndef __MATRIX_H__
+#define __MATRIX_H__
+
+void _74HC595_WriteByte(unsigned char Byte);
+void MatrixLED_ShowColun(unsigned char Column,Date);
+void MatrixLED_Init();
+
+#endif
